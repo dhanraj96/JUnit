7ad5b147e3a96d5b606cc36362e17dbf29f0e4f3
Admin <Admin@DESKTOP-7E78KSE>
2022-03-02 17:39:01 +0530

    JUnit Basics

---
+++ JunitDemo/src/Demo2.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+public class Demo2 {
+@Test
+void display()
+{
+	System.out.println("Welcome to C2TC");
+	System.out.println("Hello to Everyone");
+	
+}
+@Test
+void display1()
+{
+	System.out.println("Welcome to C2TC");
+	System.out.println("Hello to Everyone");
+}
+}
---
+++ JunitDemo/src/Demo4.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class Demo4 {
+	@Test
+	void display()
+	{
+		System.out.println("Welcome to C2TC");
+		System.out.println("Hello to Everyone");
+		
+	}
+	@Disabled
+	@Test
+	void display1()
+	{
+		System.out.println("Welcome to C2TC");
+		System.out.println("Hello to Everyone");
+	}
+	}
+
+
